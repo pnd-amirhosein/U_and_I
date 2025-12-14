e787938a2ddd870bcc408e07cb658cf8c6af5ad8
amirhoseinMardani <amir@hosein.mardani>
2025-12-14 11:39:55 +0330

    ADDED more raduis

--- ensemble-ui/dist-tokens/radii.js
+++ ensemble-ui/dist-tokens/radii.js
@@ -6,5 +6,9 @@ exports.radii = {
     sm: '2px',
     md: '4px',
     lg: '8px',
+    '2xl': '12px',
+    '3xl': '16px',
+    '4xl': '20px',
+    '5xl': '24px',
     full: '9999px',
 };
--- ensemble-ui/packages/core/tokens/radii.ts
+++ ensemble-ui/packages/core/tokens/radii.ts
@@ -3,5 +3,9 @@ export const radii = {
   sm: '2px',
   md: '4px',
   lg: '8px',
+  '2xl': '12px',
+  '3xl': '16px',
+  '4xl': '20px',
+  '5xl': '24px',
   full: '9999px',
 };
